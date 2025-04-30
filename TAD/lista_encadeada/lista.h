7389f4d25e349aac9b94bdc1bbf9aff5d0b569a9
antoniooseas <antoniooseas@ufpi.edu.br>
2025-04-30 11:21:46 -0300

    feat: novos prototipos de funcoes

--- TAD/lista_encadeada/lista.h
+++ TAD/lista_encadeada/lista.h
@@ -7,6 +7,8 @@ No *criarLista();
 No *inserirElementoInicio(No* lista);
 No *inserirElementoFinal(No* lista);
 No *inserirElementoOrdenado(No* lista);
+void liberarLista(No *lista);
+No *buscarElemento(No *lista, int valor);
 void mostrarLista(No *lista);
 
 #endif
